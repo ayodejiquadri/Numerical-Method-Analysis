8f634d11b349eea905678ac23b08fab461f992be
Ayo Quadri <ayodejiquadri30@gmail.com>
2017-10-27 00:36:31 +0100

    Added code for polynomial differentiation and Integration

--- interpolation.c
+++ interpolation.c
@@ -16,6 +16,8 @@ void interpolation(){
 	int degree =  0;
 	double b =0;
 	double x =0;
+	double derivation = 0;
+	double integral =0;
 	int i;
 	
 	printf("Enter the degree of the polynomial: ");
@@ -40,4 +42,31 @@ void interpolation(){
 	}
 	
 	printf("\n\nThe value of P(%f): %f", x, b);		
+	
+	printf("\n\n--------------------");
+	
+	derivation = degree * coefficients[degree];
+	printf("\nD(%d): %f ", degree-1, derivation);
+	
+	for(i = degree-1; i >= 1; i--){
+		derivation = i*coefficients[i] + derivation*x;
+		printf("\nD(%d): %f", i-1, derivation);
+	}
+	
+	printf("\n\nThe value of P'(%f): %f", x, derivation);
+	
+	printf("\n\n--------------------");
+	
+	integral = coefficients[degree]/(degree+1);
+	
+	printf("\nI(%d): %f ", degree+1, integral);
+	
+	for(i = degree; i >= 1; i--){
+		integral = coefficients[i-1]/i + integral*x;
+		printf("\nI(%d): %f", i, integral);
+	}
+	
+	integral = 0 + integral * x;
+	
+	printf("\n\nThe value of I(%f): %f", x, integral);	
 }
